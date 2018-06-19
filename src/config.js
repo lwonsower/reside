bf081739d2108f533bf3a9a4e94696982f8f927d
lwonsower <lwonsower@gmail.com>
2018-06-19 08:56:01 -0700

    remove cached ignored files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyDwhjREvC13DdohT3qRY5Nf4JC85id8sVU",
-  authDomain: "reside-6c8cd.firebaseapp.com",
-  databaseURL: "https://reside-6c8cd.firebaseio.com",
-  projectId: "reside-6c8cd",
-  storageBucket: "reside-6c8cd.appspot.com",
-  messagingSenderId: "965518465223",
-};
