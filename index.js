88b55e1677c25eaa28459823d55d2ef0cdd56107
lwonsower <lwonsower@gmail.com>
2018-06-19 08:52:14 -0700

    Update whitespace and gitignore

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express 	= require('express');
-const webpack 	= require('webpack');
-const config 		= require('./webpack.config');
+const express   = require('express');
+const webpack   = require('webpack');
+const config    = require('./webpack.config');
 const path      = require('path');
 
-const app 			= express();
+const app       = express();
 const compiler  = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-	noInfo: true,
-	publicPath: config.output.publicPath,
+  noInfo: true,
+  publicPath: config.output.publicPath,
 }));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build/index.html'))
+  res.sendFile(path.join(__dirname, 'build/index.html'))
 });
 
 app.listen(3000, 'localhost', (err) => {
-	if (err) {
-		return console.log(err);
-	}
-	console.log('Listening on port 3000');
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Listening on port 3000');
 });
